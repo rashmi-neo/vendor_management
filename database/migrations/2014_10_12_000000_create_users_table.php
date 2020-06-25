24d7007f0768e627d649aa7d1267d45839a22a53
bharti <neha.bhole@neosofttech.com>
2020-06-25 14:56:03 +0530

    updated user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,13 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique()->length('50');
-            $table->string('username')->length('50');
             $table->string('password')->length('100');
-            $table->integer('role_id')->unsigned();
-            $table->foreign('role_id')->references('id')->on('vms_roles');
-            $table->boolean('is_verified');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
---
+++ database/migrations/2020_06_25_085809_add_email_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEmailToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('role_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('vms_roles')->onDelete('cascade');
+            $table->string('username')->length('50');
+            $table->boolean('is_verified');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            
+        });
+    }
+}

e80acef78d1bc3ccda7ecdc5e5979cc94a385ddb
rashmi-neo <rashmi.narvare.neo@gmail.com>
2020-07-02 20:44:19 +0530

    created the delete funtion in custom js,integrated the multiselect with checkbox in requirement form

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -42,30 +42,6 @@ $(function () {
     });
     
   });
-
-function delete_com(id){
- 	var catid = id;
-  	$.confirm({
-        title: 'Confirmation',
-        content: 'Are You Sure?',
-        buttons: {
-            confirm: function () {
-                $.ajax({
-                    id: catid,
-        			type: 'DELETE',
-                    url: "/admin/categories/"+catid,
-                    data: {_token:  '{{ csrf_token() }}' , id: catid  },
-                       success: function(result){
-                         window.location.replace('/admin/categories'); 
-                    }
-                });
-            },
-            cancel: function () {
-                    
-                } 
-        }
-    });
- }
  $(document).ready(function(){
         $("[rel=tooltip]").tooltip();
     });

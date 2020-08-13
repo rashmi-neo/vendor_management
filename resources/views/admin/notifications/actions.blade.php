c227a0033c0d9f730ec4b417a45a4df786f7700e
bharti <neha.bhole@neosofttech.com>
2020-08-13 10:40:38 +0530

    Fixed changes

--- resources/views/admin/notifications/actions.blade.php
+++ resources/views/admin/notifications/actions.blade.php
@@ -4,7 +4,7 @@
 		<a href="{{route('vendors.index')}}" class="view btn btn-success btn-sm" rel="tooltip" title="View"><i class="fas fa-eye"></i></a>&nbsp;
 		@endif
 		@if($row->type=='Document update')
-		<a href="{{ url('admin/showQuotation/1/2')}}" class="view btn btn-success btn-sm" rel="tooltip" title="View"><i class="fas fa-eye"></i></a>&nbsp;
+		<a href="{{route('requirements.index')}}" class="view btn btn-success btn-sm" rel="tooltip" title="View"><i class="fas fa-eye"></i></a>&nbsp;
 		@endif
 		@if($row->type=='New requirement')
 		<a href="{{route('new.requirement.index')}}" class="view btn btn-success btn-sm " rel="tooltip" title="View"><i class="fas fa-eye"></i></a>&nbsp;

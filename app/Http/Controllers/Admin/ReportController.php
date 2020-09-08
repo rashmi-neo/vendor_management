b1ab4d85b849808fbd52e4d8b4f1a2ef571e78ef
bharti <neha.bhole@neosofttech.com>
2020-09-08 19:34:39 +0530

    Changed background color in reports

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -25,6 +25,7 @@ class ReportController extends Controller
         
         $countData=[];
         $dateWiseData =[];
+        $fillColors = [];
 
         $vendors = Vendor::select(\DB::raw("COUNT(*) as count"))
         ->pluck('count');
@@ -44,40 +45,10 @@ class ReportController extends Controller
         foreach($totalRequirementdate as $date){
             
             $dateWiseData[] = date("j-M-y", strtotime($date));
+            $fillColors[] = $this->random_color();
         }
-
-        foreach($totalRequirement as $count){
-            
-            $countData[] = $count ;
-        }
-        
+       
         $assignRequirement = AssignVendor::count();
-
-        $borderColors = [
-            "rgba(255, 99, 132, 1.0)",
-            "rgba(22,160,133, 1.0)",
-            "rgba(255, 205, 86, 1.0)",
-            "rgba(51,105,232, 1.0)",
-            "rgba(244,67,54, 1.0)",
-            "rgba(34,198,246, 1.0)",
-            "rgba(153, 102, 255, 1.0)",
-            "rgba(255, 159, 64, 1.0)",
-            "rgba(233,30,99, 1.0)",
-            "rgba(205,220,57, 1.0)"
-        ];
-        $fillColors = [
-            "rgb(92, 219, 148)",
-            "rgb(255, 99, 132)",
-            "rgb(43, 79, 213)",
-            "rgb(255, 205, 86)",
-            "rgb(51,105,232)",
-            "rgb(244,67,54)",
-            "rgb(34,198,246)",
-            "rgb(153, 102, 255)",
-            "rgb(255, 159, 64)",
-            "rgb(233,30,99)",
-            "rgb(205,220,57)",
-        ];
 
         $chart = new ReportChart;
         $chart->minimalist(true);
@@ -90,16 +61,28 @@ class ReportController extends Controller
         $completedRequirementChart->minimalist(true);
         $completedRequirementChart->labels(['Assigned Requirements','Completed Requirements']);
         $completedRequirementChart->dataset('Vendors', 'doughnut', [$assignRequirement,$completedRequirement])
-            ->color($borderColors)
             ->backgroundcolor(['#e83e8c','#28a745']);
     
         $totalRequirementChart  = new ReportChart;
         $totalRequirementChart->minimalist(false);
         $totalRequirementChart->labels($dateWiseData);
-        $totalRequirementChart->dataset('Requirements', 'bar',$countData)
+        $totalRequirementChart->dataset('Requirements', 'bar',$totalRequirement)
             ->backgroundcolor($fillColors);
 
         return view('admin.reports.index',compact('chart','completedRequirementChart','totalRequirementChart'));
         
     }
+
+    /**
+    * Create ramdom colors.
+    *@author Bharti<bharti.tadvi@neosofttech.com>
+    *
+    *@param  void
+    *@return $colors
+    */
+    public function random_color() {
+        $colors = '#' . str_pad(dechex(mt_rand(0, 0xFFFFFF)), 6, '0', STR_PAD_LEFT);
+        return $colors;
+    }
+    
 }
